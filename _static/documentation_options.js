9fd0a31f4604a94a020593f0a3bbd87d0f478d60
agriyakhetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-06-24 13:29:13 +0000

    deploy: af73caaee0a5a94ece12c8e683904ad90e8b3e2d

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.1.0.dev0+git20240618.aa40795',
+    VERSION: '2.1.0.dev0+git20240624.af73caa',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
